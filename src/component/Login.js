d779e1275d89512d771011c9fd750319c5f0ec07
DaeWon9 <qkreodnjs97@naver.com>
2022-10-13 20:48:11 +0900

    [ADD] getMemberData

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,6 +1,7 @@
 import './Login.css';
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import { getMemberData } from '../data/MemberData.js';
 
 const Login = function () {
 
@@ -8,46 +9,15 @@ const Login = function () {
     let [password, setPassword] = useState("");
     let navigate = useNavigate();
 
-    let userData = [
-        {
-            userEmail: 'test1@naver.com',
-            userPassword: 'qwe123!',
-            userName: '박대원'
-        },
-        {
-            userEmail: 'test2@naver.com',
-            userPassword: 'qwe123@',
-            userName: '조형준'
-        },
-        {
-            userEmail: 'test3@naver.com',
-            userPassword: 'qwe123#',
-            userName: '박민지'
-        },
-        {
-            userEmail: 'test3@naver.com',
-            userPassword: 'qwe123#',
-            userName: '홍영환'
-        },
-        {
-            userEmail: 'test3@naver.com',
-            userPassword: 'qwe123#',
-            userName: '조준희'
-        },
-        {
-            userEmail: 'test3@naver.com',
-            userPassword: 'qwe123#',
-            userName: '김영림'
-        },
-    ];
-
     function checkLoginSuccess() {
+        let userData = getMemberData();
+
         for (let userIndex = 0; userIndex < userData.length; userIndex++) {
-            if (email == userData[userIndex].userEmail && password == userData[userIndex].userPassword){
+            if (email == userData[userIndex].memberEmail && password === userData[userIndex].memberPassword){
                 navigate("/workSpace", { state:
                     {   
-                        loginUserEmail : userData[userIndex].userEmail,
-                        loginUserName: userData[userIndex].userName
+                        loginUserEmail : userData[userIndex].memberEmail,
+                        loginUserName: userData[userIndex].memberName
                     } 
                 })
                 return;
@@ -59,39 +29,39 @@ const Login = function () {
     return(
         <div className="Auth-form-container">
             <form className="Auth-form">
-            <div className="Auth-form-content">
-                {/* <h3 className="Auth-form-title">로그인</h3> */}
-                <div className="form-group mt-3">
-                <label>이메일</label>
-                <input
-                    type="email"
-                    className="form-control mt-1"
-                    placeholder="이메일을 입력해주세요"
-                    value={ email }
-                    onChange={e => setEmail(e.target.value)}
-                />
+            <h3 className="Auth-form-title">Lobster</h3>
+                <div className="Auth-form-content">
+                    <div className="form-group mt-3">
+                        <label>이메일</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="이메일을 입력해주세요"
+                            value={ email }
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>비밀번호</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="비밀번호를 입력해주세요"
+                            value={ password }
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                
+                    <button type="submit" className="btn btn-primary" onClick={ checkLoginSuccess }>
+                        로그인
+                    </button>
+                
+                    </div>
+                    <p className="forgot-password text-right mt-2">
+                        <a href="#">비밀번호를 잊어버리셨습니까?</a>
+                    </p>
                 </div>
-                <div className="form-group mt-3">
-                <label>비밀번호</label>
-                <input
-                    type="password"
-                    className="form-control mt-1"
-                    placeholder="비밀번호를 입력해주세요"
-                    value={ password }
-                    onChange={e => setPassword(e.target.value)}
-                />
-                </div>
-                <div className="d-grid gap-2 mt-3">
-            
-                <button type="submit" className="btn btn-primary" onClick={ checkLoginSuccess }>
-                    로그인
-                </button>
-            
-                </div>
-                <p className="forgot-password text-right mt-2">
-                    <a href="#">비밀번호를 잊어버리셨습니까?</a>
-                </p>
-            </div>
             </form>
         </div>
     );
