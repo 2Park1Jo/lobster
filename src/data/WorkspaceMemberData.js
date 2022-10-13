041eb37d84fc5a3c2bc5ac1970d231301bc2bf97
DaeWon9 <qkreodnjs97@naver.com>
2022-10-13 20:47:20 +0900

    [ADD] add dummyData

---
+++ src/data/WorkspaceMemberData.js
@@ -0,0 +1,43 @@
+let workspaceMemberData = [
+    {
+        memberEmail: 'test1@naver.com',
+        workspaceId: '1',
+        memberName: '박대원',
+        memberGrade: '',
+    },
+    {
+        memberEmail: 'test2@naver.com',
+        workspaceId: '1',
+        memberName: '조형준',
+        memberGrade: '',
+    },
+    {
+        memberEmail: 'test3@naver.com',
+        workspaceId: '1',
+        memberName: '박민지',
+        memberGrade: '',
+    },
+    {
+        memberEmail: 'test4@naver.com',
+        workspaceId: '1',
+        memberName: '홍영환',
+        memberGrade: '',
+    },
+    {
+        memberEmail: 'test5@naver.com',
+        workspaceId: '1',
+        memberName: '조준희',
+        memberGrade: '',
+    },
+    {
+        memberEmail: 'test6@naver.com',
+        workspaceId: '1',
+        memberName: '김영림',
+        memberGrade: '',
+    },
+];
+
+export function getWorkspaceMemberData() {
+    return workspaceMemberData;
+}
+
