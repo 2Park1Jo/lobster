1564ddc66603a99aa67011046bb9c0829e7656db
DaeWon9 <qkreodnjs97@naver.com>
2022-10-26 02:04:04 +0900

    [ADD] add sample data

--- src/data/WorkspaceData.js
+++ src/data/WorkspaceData.js
@@ -10,7 +10,31 @@ let workspaceData = [
         workspaceName: '데이터베이스 과제방',
         workspaceGoal: '데이터베이스 발표 PPT 완성',
         workspaceDeadline: '2022-10-30'
-    }
+    },
+    {
+        workspaceId: '3',
+        workspaceName: '대원이와 아이들 과제',
+        workspaceGoal: '공모전 상타기',
+        workspaceDeadline: '2023-01-30'
+    },
+    {
+        workspaceId: '4',
+        workspaceName: '형준이는 치타일까',
+        workspaceGoal: '형준이에 대한 연구',
+        workspaceDeadline: '2022-10-30'
+    },
+    {
+        workspaceId: '5',
+        workspaceName: '엔샵 동아리방',
+        workspaceGoal: '스터디 완수',
+        workspaceDeadline: '2022-10-30'
+    },
+    {
+        workspaceId: '6',
+        workspaceName: '세종대 축제준비방',
+        workspaceGoal: '축제 성공적으로 끝내기',
+        workspaceDeadline: '2022-10-30'
+    },
 ];
 
 export function getWorkspaceData() {
