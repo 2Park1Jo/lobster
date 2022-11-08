204dea918b568c8f8fe1ff5c6062e2afd1e59587
DaeWon9 <qkreodnjs97@naver.com>
2022-11-08 23:07:03 +0900

    [ADD] Message 허용범위 넘어가면 아래로 넘어가도록 추가

--- src/components/workspace/chat/Message.js
+++ src/components/workspace/chat/Message.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { ListGroup } from 'react-bootstrap'
+import './Message.css';
 
 
 export default function Message({ chatSender, chatDate, chatContent }){
     return(
-        <div>
+        <div className='message'>
             <li className="small text-muted">{ chatSender } { chatDate }</li>
             <ListGroup.Item action variant="dark" className="rounded">
                 <span className="small"> { chatContent } </span>
