2643044249a7b69cda1a25c4aff468020a39c29d
DaeWon9 <qkreodnjs97@naver.com>
2022-11-11 20:41:11 +0900

    [ADD] bucket, modifymodal

---
+++ src/components/workspace/Bucket.js
@@ -0,0 +1,10 @@
+import '../../pages/Workspace.css'
+
+export default function Bucket(){
+
+    return(
+        <div className="contents-container">
+            asdgasdgasdgasdgasdgasdgasdgasdg
+        </div>
+    );
+}
