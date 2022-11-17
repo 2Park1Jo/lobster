036db589661498f4a3f1aa5e3b66b39a64965147
DaeWon9 <qkreodnjs97@naver.com>
2022-11-18 00:41:29 +0900

    [UPDATE] design

--- src/components/modals/DepartmentMemberAddModal.js
+++ src/components/modals/DepartmentMemberAddModal.js
@@ -1,48 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { ListGroup } from "react-bootstrap";
-import Form from 'react-bootstrap/Form';
-
 import './Modal.css';
 
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-<a
-    href=""
-    ref={ref}
-    onClick={(e) => {
-    e.preventDefault();
-    onClick(e);
-    }}
->
-    {children}
-    &#x25bc;
-</a>
-));
-
 const CustomMenu = React.forwardRef(
-({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
+({ children, style, className, 'aria-labelledby': labeledBy, selectedMemberNameList }, ref) => {
     const [value, setValue] = useState('');
 
     return (
-    <div
-        ref={ref}
-        style={style}
-        className={className}
-        aria-labelledby={labeledBy}
-    >
-        <Form.Control
-        autoFocus
-        className="mx-3 my-2 w-auto"
-        placeholder="멤버 검색"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-        />
-        <ul className="list-unstyled">
-        {React.Children.toArray(children).filter(
-            (child) =>
-            !value || child.props.children.toLowerCase().startsWith(value),
-        )}
-        </ul>
+    <div>
+        <div className='selected-member-container'>
+            <span className='selected-member-container'>
+                {selectedMemberNameList}
+            </span>
+            <input
+                style={{backgroundColor:'gainsboro', border:'none', width:'auto', outline:'none'}}
+                placeholder="멤버 검색"
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+            />
+        </div>
+
+        <div ref={ref} className={className} aria-labelledby={labeledBy}>
+            <ul className="list-unstyled" style={{width:'280px', height:'200px', overflowY:'auto'}}>
+                {React.Children.toArray(children).filter(
+                    (child) =>
+                    !value || child.props.children.includes(value),
+                )}
+            </ul>
+        </div>
     </div>
     );
 },
@@ -54,13 +39,21 @@ const DepartmentMemberAddModal = ({modalIsOpen, setModalIsOpen, accessedDepartme
     let workspaceMemberData = workspaceMembers;
 
     useEffect( () => {
-        console.log(inputDepartmentMemberData)
         setSelectedMemberName()
     }, [inputDepartmentMemberData])
 
     function isAlreadyJoinMember(memberEmail){
         for (let index = 0; index < departmentMembers.length; index++) {
             if (memberEmail === departmentMembers[index].email){
+                return true
+            }
+        }
+        return false
+    }
+
+    function isSelectedMember(memberEmail){
+        for (let index = 0; index < selectedMemberNameList.length; index++){
+            if (memberEmail === selectedMemberNameList[index].key){
                 return true
             }
         }
@@ -75,9 +68,16 @@ const DepartmentMemberAddModal = ({modalIsOpen, setModalIsOpen, accessedDepartme
             let memberEmail = workspaceMemberData[index].email
 
             if (!isAlreadyJoinMember(memberEmail)){
-                htmlArrayForDepartmentMember.push(
-                    <Dropdown.Item key={ memberEmail } eventKey={ memberEmail } onClick={ () => addMemberData(memberName, memberEmail) }>{ memberName }</Dropdown.Item>
-                )
+                if (isSelectedMember(memberEmail)){
+                    htmlArrayForDepartmentMember.push(
+                        <Dropdown.Item style={{backgroundColor:'gainsboro'}} className='dropdown-member-div' key={ memberEmail } eventKey={ memberEmail } onClick={ () => addMemberData(memberName, memberEmail) }>{ memberName + " (" + memberEmail + ")"}</Dropdown.Item>
+                    )
+                }
+                else{
+                    htmlArrayForDepartmentMember.push(
+                        <Dropdown.Item className='dropdown-member-div' key={ memberEmail } eventKey={ memberEmail } onClick={ () => addMemberData(memberName, memberEmail) }>{ memberName + " (" + memberEmail + ")"}</Dropdown.Item>
+                    )
+                }
             }
         }
         return htmlArrayForDepartmentMember
@@ -122,8 +122,8 @@ const DepartmentMemberAddModal = ({modalIsOpen, setModalIsOpen, accessedDepartme
 
             memberNameList.push(
                 <span key={email} className='selected-member-div' onClick={() => deleteMemberInList(email)}>
-                    <span style={{color:'black', fontSize : '16px'}}>{memberName}</span>
-                    <span style={{color:'black', fontSize : '10px'}}>X</span>
+                    <span style={{color:'black', fontSize : '16px', marginLeft:'5px'}}>{memberName}</span>
+                    <span style={{color:'black', fontSize : '12px', marginRight:'5px'}}> X</span>
                 </span>
             )
         }
@@ -156,23 +156,18 @@ const DepartmentMemberAddModal = ({modalIsOpen, setModalIsOpen, accessedDepartme
     }
 
     return(
-        <div style={{width:'300px', height:'300px'}}>
+        <div className='departmentMember-add-modal-container'>
             <button className="modal-close" type="button" onClick={() => setModalIsOpen(false)}>X</button>
             <h3 className="Auth-form-title">멤버추가</h3>
-            <div className="form-group mt-3">
-                <div className='selected-member-container'>
-                    {selectedMemberNameList}
-                </div>
-                <Dropdown>
-                    <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components"></Dropdown.Toggle>
-                    <Dropdown.Menu as={CustomMenu}>
+            <div className="form-group mt-3" style={{height:'300px'}}>
+                <Dropdown show={true}>
+                    <Dropdown.Menu as={CustomMenu} selectedMemberNameList={selectedMemberNameList}>
                         { applyWorkspaceMemberListInDropdown() }
                     </Dropdown.Menu>
-                    &nbsp;멤버추가
                 </Dropdown>
             </div>
 
-            <div className="d-grid gap-2 mt-3" style={{position: 'absolute', width:'90%', left: '5%', bottom: '10px'}}>
+            <div className="d-grid gap-2 mt-3">
                 <button className="btn btn-primary" onClick={ () => addDepartmentData() }>
                     추가하기
                 </button>
