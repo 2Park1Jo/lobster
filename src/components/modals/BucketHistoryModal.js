de17e1900ec9a38b59b1bed5593729fddb729654
RecordOfJun <pritty0731@naver.com>
2022-11-27 15:14:12 +0900

    [FIX]workspace 버킷내역 디자인 수정

--- src/components/modals/BucketHistoryModal.js
+++ src/components/modals/BucketHistoryModal.js
@@ -2,11 +2,13 @@ import { FileList } from "../workspace/FileList"
 import { AiOutlineFileText } from "react-icons/ai"
 import {getBucket} from "../../api/BucketAPI.js"
 import { useState,useEffect,useRef } from "react"
+import BucketSemiCard from "../workspace/BucketSemiCard.js"
 const BucketHistoryModal=({setBucketMenu,departmentId})=>{
     const [bucketList,setBucketList]=useState([]);
     const [title,setTitle]=useState("")
     const [fileList,setFileList]=useState([])
     const [detail,setDetail]=useState("")
+    const fileLink=useRef()
 
     let bucket=[]
 
@@ -38,11 +40,14 @@ const BucketHistoryModal=({setBucketMenu,departmentId})=>{
                     }
 
                     list.push(
-                    <div className="bucket-history-modal-commit" onClick={()=>showHistory(i)}>
-                        <span style={{fontSize:"20px"}}>{data[i].title}</span>
-                        <span style={{fontSize:"12px"}}>파일개수 : {fileCount}</span>
-                        <span style={{fontSize:"12px"}}>{data[i].date}</span>
-                    </div>)
+                        <BucketSemiCard
+                        bucketTitle = {data[i].title}
+                        memberName = {data[i].memberName}
+                        email = {data[i].email}
+                        date = {data[i].date}
+                        onClick={() => showHistory(i)}
+                        key = {i}
+                      />)
                 }
                 setBucketList([...list])
             }
@@ -59,27 +64,27 @@ const BucketHistoryModal=({setBucketMenu,departmentId})=>{
             setTitle(bucket[index].title);
             if(bucket[index].fileLink1!==null){
                         list.push(
-                        <a className="file-container" href={bucket[index].fileLink1} >
+                        <a className="file-container" href={bucket[index].fileLink1} ref={fileLink} >
                         <AiOutlineFileText style={{fontSize:"50px", color:"black"}}/>
                         <div className="content">{bucket[index].fileLink1.substring(bucket[index].fileLink1.lastIndexOf("/")+1)}</div>
                         </a>)
                         if(bucket[index].fileLink2!==null){
-                            list.push(<a className="file-container" href={bucket[i].fileLink2}>
+                            list.push(<a className="file-container" href={bucket[i].fileLink2} ref={fileLink}>
                             <AiOutlineFileText style={{fontSize:"50px", color:"black"}}/>
                             <div className="content">{bucket[index].fileLink2.substring(bucket[index].fileLink2.lastIndexOf("/")+1)}</div>
                             </a>)
                             if(bucket[index].fileLink3!==null){
-                                list.push(<a className="file-container" href={bucket[i].fileLink3}>
+                                list.push(<a className="file-container" href={bucket[i].fileLink3} ref={fileLink[2]}>
                                 <AiOutlineFileText style={{fontSize:"50px", color:"black"}}/>
                                 <div className="content">{bucket[index].fileLink3.substring(bucket[index].fileLink3.lastIndexOf("/")+1)}</div>
                                 </a>)
                                 if(bucket[index].fileLink4!==null){
-                                    list.push(<a className="file-container" href={bucket[i].fileLink4}>
+                                    list.push(<a className="file-container" href={bucket[i].fileLink4} ref={fileLink[3]}>
                                     <AiOutlineFileText style={{fontSize:"50px", color:"black"}}/>
                                     <div className="content">{bucket[index].fileLink4.substring(bucket[index].fileLink4.lastIndexOf("/")+1)}</div>
                                     </a>)
                                     if(bucket[index].fileLink5!==null){
-                                        list.push(<a className="file-container" href={bucket[index].fileLink5}>
+                                        list.push(<a className="file-container" href={bucket[index].fileLink5} ref={fileLink[4]}>
                                         <AiOutlineFileText style={{fontSize:"50px", color:"black"}}/>
                                         <div className="content">{bucket[index].fileLink5.substring(bucket[index].fileLink5.lastIndexOf("/")+1)}</div>
                                         </a>)
@@ -92,6 +97,14 @@ const BucketHistoryModal=({setBucketMenu,departmentId})=>{
         setDetail(bucket[index].commit)
             }
     }
+    
+    function fileDownload(){
+        console.log(fileLink.length)
+        for(var i=0;i<fileLink.length;i++){
+            console.log(i)
+            fileLink.current[i].click();
+        }
+    }
     return(
         <>
         <div>
@@ -101,10 +114,10 @@ const BucketHistoryModal=({setBucketMenu,departmentId})=>{
         <div className="bucket-history-modal-container">
                     <div className="bucket-history-modal-commitsContainer">
                         {bucketList.length===0?
-                            <>
-                                <span style={{color:"black", fontSize:"30px"}}>버킷이 비어있습니다!</span>
-                                <span style={{color:"black", fontSize:"20px",textAlign:"center"}}>버킷에 자료를 올리고 구성원들과 공유해보세요!</span>
-                            </>
+                            <div className="bucket-history-empty">
+                                <span style={{color:"white", fontSize:"30px"}}>버킷이 비어있습니다!</span>
+                                <span style={{color:"white", fontSize:"20px",textAlign:"center"}}>버킷에 자료를 올리고 구성원들과 공유해보세요!</span>
+                            </div>
                             :
                         <div className="bucket-history-modal-commitList">
                                 {bucketList}
@@ -118,24 +131,29 @@ const BucketHistoryModal=({setBucketMenu,departmentId})=>{
                         :
                             <>
                             <div className="bucket-history-modal-selected-container">
-                                <div style={{flexDirection:"row", marginTop:"30px"}}>
-                                <span style={{float:"left", marginLeft:"20px", fontSize:"30px"}}>제목</span>
-                                <input type="text" className="w-75 form-control bg-light" value={ title }
-                                style={{paddingBottom:"10px", marginLeft:"105px"}} readonly="readonly"/>
+                            <div style={{width:"600px",height:"95%"}}>
+                            <div className="bucket-page-box-container">
+                                <div style={{display:'inline-block'}}>
+                                    제목
                                 </div>
 
-                                <div className="bucket-history-modal-file" style={{marginTop:"40px"}}>
-                                    {fileList}
+                                <div className="bucket-page-box-title">
+                                    {title}
+                                </div>
+
+                                <div className="bucket-page-box-files">{fileList}</div>
+
+                                <div style={{display:'inline'}}>
+                                    내용
                                 </div>
                                 
-                                <div style={{flexDirection:"row",marginTop:"40px"}}>
-                                <span style={{float:"left" , marginLeft:"10px",fontSize:"20px"}}>세부사항</span>
-                                <textarea style={{resize: 'none', float:"left", height:"200px",marginLeft:"30px"}} type="text" className="w-75 form-control bg-light" readonly="readonly" value={detail}/>
+                                <div className="bucket-page-box-commit">
+                                    {detail}
                                 </div>
-                                <div>
-                                <button className="btn btn-danger" style={{float:"right", marginTop:"20px", marginRight:"61px", marginBottom:"20px"}}>파일 다운로드</button>
-                                </div>
-                                </div>
+                                <button className="btn btn-danger" style={{float:"right", marginTop:"20px", marginRight:"61px", marginBottom:"20px"}} onClick={()=>fileDownload()}>파일 다운로드</button>
+                            </div>
+                            </div>
+                            </div>
                             </>
                         }
         </div>
